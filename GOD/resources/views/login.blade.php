1ed8f9bac9e96a0f06879717f4d7169fa5725c3b
RubenCardoso30344 <a30344@aelourinha.pt>
2021-04-13 10:21:08 +0100

    aaaa

--- GOD/resources/views/login.blade.php
+++ GOD/resources/views/login.blade.php
@@ -22,12 +22,7 @@
       body
       {
         min-height: 100vh;
-<<<<<<< HEAD
         /*aaa*/
-=======
-        background-color: #fff;/* es gay*/
-        background-color: #000;
->>>>>>> main
       }
 
       .navbar
