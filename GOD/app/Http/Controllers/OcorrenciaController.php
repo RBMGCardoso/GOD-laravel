007c2a586cff7bb6a4a4e3ca67307314e555d29c
RubenCardoso30344 <a30344@aelourinha.pt>
2021-04-22 14:20:52 +0100

    Sistema de cargos(atribuição) Armazenamento de motivos por ocorrencia

---
+++ GOD/app/Http/Controllers/OcorrenciaController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ocorrencia;
+use App\Models\Aluno;
+use Illuminate\Http\Request;
+
+class OcorrenciaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    
+    public function index(Ocorrencia $ocorrencia)
+    {
+        $ocorrencias = Ocorrencia::all()->reverse();
+        return view('pesquisa', ['ocorrencias' => $ocorrencias]);  
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ocorrencia  $ocorrencia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ocorrencia $ocorrencia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ocorrencia  $ocorrencia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ocorrencia $ocorrencia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ocorrencia  $ocorrencia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ocorrencia $ocorrencia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ocorrencia  $ocorrencia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ocorrencia $ocorrencia)
+    {
+        //
+    }
+}
