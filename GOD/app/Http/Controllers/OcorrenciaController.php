660f61ffadecd8a8faa741bb897d44254d7a6f8b
RubenCardoso30344 <a30344@aelourinha.pt>
2021-06-08 16:27:55 +0100

    eqeq

--- GOD/app/Http/Controllers/OcorrenciaController.php
+++ GOD/app/Http/Controllers/OcorrenciaController.php
@@ -102,7 +102,7 @@ class OcorrenciaController extends Controller
     public function perfilAluno(Aluno $idAluno)
     {
         //$descricao = Ocorrencia::find($idOcc)->pluck('aluno_id');
-        dd($idAluno->datanasc);
+        dd($idAluno->morada);
         //return view('dashboard');
     }
 
--- GOD/resources/views/dashboard.blade.php
+++ GOD/resources/views/dashboard.blade.php
@@ -49,7 +49,7 @@
     
   </head>
   <body>
-    <div class="navbar-div">
+  <div class="navbar-div">
       <nav class="navbar navbar-expand d-flex flex-column align-item-start" id="sidebar">
           <a href="{{ route('dashboardPage') }}" class="navbar-brand text-light">
             <div class="display-5 font-weight-bold">
@@ -101,20 +101,20 @@
 
         <div class="profile">
           <div class="dropup ms-3">
-            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle-nav" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle me-2">
+            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="https://st2.depositphotos.com/1104517/11967/v/950/depositphotos_119675554-stock-illustration-male-avatar-profile-picture-vector.jpg" alt="mdo" width="32" height="32" class="rounded-circle me-2">
               <strong>{{ session('LoggedUser')->name }}</strong>
             </a>
             <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
               <li><a class="dropdown-item" href="#">
                 @forelse (session('LoggedUser')->cargos as $cargo)
-                  {{ $cargo->cargo }}
+                  Cargo: {{ $cargo->cargo }}
                 @empty
-                  Nenhum cargo atribuido
+                  Nenhum cargo atribuido ou sessão expirada.
                 @endforelse             
               </a></li>
-              <li><a class="dropdown-item" href="#">Settings</a></li>
-              <li><a class="dropdown-item" href="#">Profile</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Perfil</a></li>
               <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item" href="{{ route('logout') }}">Sign out</a></li>
             </ul>
@@ -125,18 +125,20 @@
       </nav>
 
       <button class="btn text-dark shadow-none" id="menu-btn" style="position:relative;left:250px;width:50px;height:50px" onclick="closeSidebar()">
-      <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-        width="30px" height="30px" viewBox="0 0 451.846 451.847" style="enable-background:new 0 0 451.846 451.847;">
-        <g>
-          <path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
-            c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
-            c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"/>
-        </g>
-      </svg>
+        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+          width="30px" height="30px" viewBox="0 0 451.846 451.847" style="enable-background:new 0 0 451.846 451.847;">
+          <g>
+            <path d="M97.141,225.92c0-8.095,3.091-16.192,9.259-22.366L300.689,9.27c12.359-12.359,32.397-12.359,44.751,0
+              c12.354,12.354,12.354,32.388,0,44.748L173.525,225.92l171.903,171.909c12.354,12.354,12.354,32.391,0,44.744
+              c-12.354,12.365-32.386,12.365-44.745,0l-194.29-194.281C100.226,242.115,97.141,234.018,97.141,225.92z"/>
+          </g>
+        </svg>
       </button>
+      
       <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
       <script src="./js/sidebars.js"></script>
     </div>
+
     <div class="content">
 
     </div>
